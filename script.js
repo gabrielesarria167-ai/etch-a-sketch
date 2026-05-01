9b6629be9ca77e5b13f9f06a8948539576f053d4
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-05-01 13:57:53 +0200

    Add container resizing functions, add cursor event listener

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-// example with 16 * 16 pixels
+function setDimensions(value) {
+    for (let i = 0; i < value; i++) {
+        let xRow = document.createElement("div");
+        xRow.classList.add("row");
+        container.appendChild(xRow);
+
+        for (let j = 0; j < value; j++) {
+            let pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            xRow.appendChild(pixel);
+        }
+    }
+}
+
+function clearContainer(container){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+function modifySize() {
+    clearContainer(container);
+    sliderPixels.textContent = `${slider.value}`;
+    setDimensions(slider.value);
+}
 
 const container = document.querySelector("#container");
 
-for(let i = 0; i<16; i++){
-    let xRow = document.createElement("div");
-    xRow.classList.add("row");
-    container.appendChild(xRow);
+let slider = document.querySelector("#slider");
+let sliderPixels = document.querySelector("#slider-dimension");
 
-    for(let j = 0; j<16; j++){
-        let pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        xRow.appendChild(pixel);
-    }
-}
+slider.value = 16;
+slider.min = 16;
+slider.max = 64;
+slider.step = 16;
+
+modifySize(container); // runs once to load first grid
+sliderPixels.textContent = `${slider.value}`; // runs once to load first grid size
+
+slider.addEventListener("click", () => {
+    modifySize();
+});
 
