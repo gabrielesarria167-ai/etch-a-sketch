126b4f540d70c2249fa08a98d6f3ccb0b0c63df3
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-05-03 02:37:36 +0200

    add rgb picker event listener

--- script.js
+++ script.js
@@ -10,37 +10,38 @@ function setDimensions(value) {
             xRow.appendChild(pixel);
         }
     }
-    totalPixels = document.querySelectorAll(".pixel");
+    setPixelsColoring(); // loads ability for pixels inside container to be colored upon hover
+}
 
+function setPixelsColoring() {
+    totalPixels = document.querySelectorAll(".pixel");
     totalPixels.forEach((pixel) => { // pixels changing color on hover start only when the mouse gets clicked
         pixel.addEventListener("click", () => {
-            colorPixel(pixel); // calls color fn once for the first btn click to color the pixel
+            colorPixel(pixel, currentColor); // calls color fn once for the first btn click to color the pixel
             totalPixels.forEach((pixel) => { // updates event listener for every dimension change
-                pixel.addEventListener("mouseover", () => {
-                    colorPixel(pixel);
-                })
+                pixel.addEventListener("mouseover", () => colorPixel(pixel, currentColor));
             })
         })
     })
 }
 
 function clearContainer(container) {
-    while (container.firstChild) {
+    while (container.firstChild) { // loops until theres no child element
         container.removeChild(container.firstChild);
     }
 }
 
 function modifySize() {
-    clearContainer(container);
+    clearContainer(container); // clears prev. container
     sliderPixels.textContent = `${slider.value}`;
-    setDimensions(slider.value);
+    setDimensions(slider.value); // creates new container with set dimensions
 }
 
-function colorPixel(pixel) {
-    pixel.style.backgroundColor = "black";
+function colorPixel(pixel, color) {
+    pixel.style.backgroundColor = `${color}`;
 }
 
- // container variables
+// container variables
 const container = document.querySelector("#container");
 let totalPixels;
 
@@ -50,10 +51,17 @@ let sliderPixels = document.querySelector("#slider-dimension");
 
 const resetBtn = document.querySelector("#reset");
 
+let rgbPicker = document.querySelector("#rgb-picker");
+let currentColor = "#000000";
+rgbPicker.value = "#000000";
+
+
 slider.value = 16;
 slider.min = 16;
 slider.max = 64;
 slider.step = 16;
+
+
 
 modifySize(); // runs once to load first grid
 sliderPixels.textContent = `${slider.value}`; // runs once to load first grid size
@@ -63,6 +71,12 @@ slider.addEventListener("click", () => {
 });
 
 resetBtn.addEventListener("click", () => {
+    rgbPicker.value = "#000000";
+    currentColor = "#000000";
     clearContainer(container); // removes all container pixels (including white ones)
-    modifySize(container); // loads container again
+    modifySize(); // loads container again
 })
+
+rgbPicker.addEventListener("input", (e) =>{
+    currentColor = e.target.value;
+} )
