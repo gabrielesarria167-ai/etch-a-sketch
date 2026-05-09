4b0aa34e35f91a3af6ec9c4679d3ba4dc110a6b5
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-05-09 19:11:21 +0200

    Add color hovering toggle with click

--- script.js
+++ script.js
@@ -13,16 +13,37 @@ function setDimensions(value) {
     setPixelsColoring(); // loads ability for pixels inside container to be colored upon hover
 }
 
+function colorPixel(pixel, color) {
+    pixel.style.backgroundColor = `${color}`;
+}
+
+function colorPixelEL(event) {
+    colorPixel(event.target, currentColor);
+}
+
+function startHover(totalPixels, event) {
+    colorPixel(event.target, currentColor);
+    totalPixels.forEach((pixel) => {
+        pixel.addEventListener("mouseover", colorPixelEL);
+    })
+    hoverStatus.textContent = "Coloring mode is currently active";
+}
+
+function stopHover(totalPixels, event) {
+    totalPixels.forEach((pixel) => {
+        pixel.removeEventListener("mouseover", colorPixelEL);
+    })
+    hoverStatus.textContent = "Coloring mode disabled";
+}
+
 function setPixelsColoring() {
     totalPixels = document.querySelectorAll(".pixel");
-    totalPixels.forEach((pixel) => { // pixels changing color on hover start only when the mouse gets clicked
-        pixel.addEventListener("click", () => {
-            colorPixel(pixel, currentColor); // calls color fn once for the first btn click to color the pixel
-            totalPixels.forEach((pixel) => { // updates event listener for every dimension change
-                pixel.addEventListener("mouseover", () => colorPixel(pixel, currentColor));
-            })
+    totalPixels.forEach((pixel) => { // pixels changing color on hover start only when the mouse gets dbl clicked
+        pixel.addEventListener("click", (e) => {
+            ongoingHover = !ongoingHover;
+            (ongoingHover) ? startHover(totalPixels, e) : stopHover(totalPixels, e);
         })
-    })
+    });
 }
 
 function clearContainer(container) {
@@ -33,13 +54,14 @@ function clearContainer(container) {
 
 function modifySize() {
     clearContainer(container); // clears prev. container
-    sliderPixels.textContent = `${slider.value}`;
+    sliderPixels.textContent = `${slider.value} x ${slider.value}`;
     setDimensions(slider.value); // creates new container with set dimensions
 }
 
-function colorPixel(pixel, color) {
-    pixel.style.backgroundColor = `${color}`;
-}
+// control variables
+
+let ongoingHover = false;
+let hoverStatus = document.querySelector("#hoverStatus");
 
 // container variables
 const container = document.querySelector("#container");
@@ -48,7 +70,6 @@ let totalPixels;
 // slider variables
 let slider = document.querySelector("#slider");
 let sliderPixels = document.querySelector("#slider-dimension");
-
 const resetBtn = document.querySelector("#reset");
 
 let rgbPicker = document.querySelector("#rgb-picker");
@@ -61,10 +82,8 @@ slider.min = 16;
 slider.max = 64;
 slider.step = 16;
 
-
-
 modifySize(); // runs once to load first grid
-sliderPixels.textContent = `${slider.value}`; // runs once to load first grid size
+sliderPixels.textContent = `${slider.value} x ${slider.value}`; // runs once to load first grid size
 
 slider.addEventListener("click", () => {
     modifySize();
@@ -77,6 +96,6 @@ resetBtn.addEventListener("click", () => {
     modifySize(); // loads container again
 })
 
-rgbPicker.addEventListener("input", (e) =>{
+rgbPicker.addEventListener("input", (e) => {
     currentColor = e.target.value;
-} )
+})
